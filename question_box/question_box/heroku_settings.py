29db2f5d943195bde3152d3cde55521b0b3bc626
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-10-23 14:25:38 -0400

    heroku file changes

--- question_box/question_box/heroku_settings.py
+++ question_box/question_box/heroku_settings.py
@@ -1,18 +1,36 @@
-from .settings import *
-import os
+# from .settings import *
+# import os
+# import dj_database_url
+#
+# DEBUG = bool(int(os.environ.get('DEBUG', False)))
+# SECRET_KEY = os.environ['SECRET_KEY']
+#
+# BLACKLIST_APPS = ['debugtoolbar', 'django_extensions']
+#
+# INSTALLED_APPS = tuple([app for app in INSTALLED_APPS if app not in BLACKLIST_APPS])
+#
+# DATABASES['default'] = dj_database_url.config()
+#
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#
+# ALLOWED_HOSTS = ['*']
+#
+# # Static asset configuration
+# import os
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+#
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# Parse database configuration from $DATABASE_URL
 import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
-DEBUG = bool(int(os.environ.get('DEBUG', False)))
-SECRET_KEY = os.environ['SECRET_KEY']
-
-BLACKLIST_APPS = ['debugtoolbar', 'django_extensions']
-
-INSTALLED_APPS = tuple([app for app in INSTALLED_APPS if app not in BLACKLIST_APPS])
-
-DATABASES['default'] = dj_database_url.config()
-
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+# Allow all host headers
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
@@ -21,4 +39,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
