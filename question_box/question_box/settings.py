124e957b4d9967178d7cdfe3c24d6355dfccfe7a
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-12-16 12:34:58 -0500

    setting updated

--- question_box/question_box/settings.py
+++ question_box/question_box/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +26,7 @@ SECRET_KEY = '880hq+z-&(4jn1tl34+^d7b09n$frizh4j25ix@$$v^b3jtjam'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,6 +105,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+DATABASES['default'] = dj_database_url.config()
+
+DATABASES['default']['ENGINE'] = 'django_postgrespool'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
